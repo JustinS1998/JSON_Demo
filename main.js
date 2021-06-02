56784cf7ba7c7cc95e147403cb380f12053d4222
Justin Seitz <justinseitz2016@gmail.com>
2021-06-02 13:12:43 -0700

    now populates super hero information, added css alignment

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ request.responseType = 'json';
 request.send();
 
 //Wait for a respond, then deal with data
-request.onload = function() {
+request.onload = function () {
     const superHeroes = request.response;
     populateHeader(superHeroes);
     showHeroes(superHeroes);
 }
 
-function populateHeader (obj) {
+function populateHeader(obj) {
     const myH1 = document.createElement('h1');
     myH1.textContent = obj['squadName'];
     myH1.style.fontFamily = '\'Orbitron\', sans-serif';
@@ -28,5 +28,36 @@ function populateHeader (obj) {
 }
 
 function showHeroes(obj) {
-    
+    const heroes = obj['members'];
+
+    for (let i = 0; i < heroes.length; i++) {
+        const myArticle = document.createElement('article');
+        const myH2 = document.createElement('h2');
+        const myPara1 = document.createElement('p');
+        const myPara2 = document.createElement('p');
+        const myPara3 = document.createElement('p');
+        const myList = document.createElement('ul');
+
+        myArticle.style.margin = '5%';
+        myH2.textContent = heroes[i].name;
+        myH2.style.fontFamily = '\'Orbitron\', sans-serif';
+        myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
+        myPara2.textContent = 'Age: ' + heroes[i].age;
+        myPara3.textContent = 'Superpowers:';
+
+        const superPowers = heroes[i].powers;
+        for (let j = 0; j < superPowers.length; j++) {
+            const listItem = document.createElement('li');
+            listItem.textContent = superPowers[j];
+            myList.appendChild(listItem);
+        }
+
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myList);
+
+        section.appendChild(myArticle);
+    }
 }
