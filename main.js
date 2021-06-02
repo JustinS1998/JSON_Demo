a6df8688c76237b9aa8d9bc62cc4a816da37466f
Justin Seitz <justinseitz2016@gmail.com>
2021-06-02 12:46:37 -0700

    now populates header from json data request

---
+++ main.js
@@ -0,0 +1,23 @@
+const header = document.querySelector('header');
+const section = document.querySelector('section');
+
+//Send a request for data
+let requestURL = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+//Wait for a respond, then deal with data
+request.onload = function() {
+    const superHeroes = request.response;
+    populateHeader(superHeroes);
+    showHeroes(superHeroes);
+}
+
+function populateHeader (obj) {
+    const myH1 = document.createElement('h1');
+    myH1.textContent = obj['squadName'];
+    myH1.style.fontFamily = '\'Orbitron\', sans-serif';
+    header.appendChild(myH1);
+}
